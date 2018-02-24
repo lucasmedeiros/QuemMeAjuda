f2213dbeee2866a9751c0610a3b5e63285ba7956
jessesouza21 <33502846+jessesouza21@users.noreply.github.com>
2018-02-24 14:23:26 -0300

    Update Alunado.java

--- QuemMeAjuda/src/aluno/Alunado.java
+++ QuemMeAjuda/src/aluno/Alunado.java
@@ -5,8 +5,9 @@ package aluno;
 
 /**
  * 
- * [DESCRIÇÃO DO QUE A INTERFACE É, REPRESENTA E OUTRAS INFORMAÇÕES IMPORTANTES]
- * 
+ * Classe que serve de Interface para a classe Aluno, que possui dois metodos para
+ * serem implementados na classe aluno
+ *
  * @authors 
  * <ol> 
  *  <i> Diego Gama </i>
@@ -17,5 +18,19 @@ package aluno;
  * @since Parte 1
  */
 public interface Alunado extends Comparable<Aluno>{
+	/**
+	 * Metodo que retorna uma String detalhada do aluno
+	 * @return retorna a representacao, em string, do aluno
+	 */
+	public String gerarDetalhes();
 
+	/**
+	 * Metodo para comparar dois Alunos
+	 * @param Aluno a ser comparado
+	 * @return retorna um inteiro, onde caso seja < 0, significa que a this.classe eh
+	 * menor que o aluno o, se for == 0 significa que sao iguais ee caso seja > 0, sig
+	 * nifica que this.classe eh maior que aluno o. Eh utilizado o nome dos alunos como
+	 * criterio de comparacao
+	 */
+	public int compareTo(Aluno o); 
 }
