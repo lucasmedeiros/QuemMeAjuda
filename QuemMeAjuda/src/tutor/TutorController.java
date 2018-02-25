de2675a9fd3289deb392f289c8594b00c3d2f217
Mikaelbfa <mikaelbamaral@gmail.com>
2018-02-25 18:57:36 -0300

    Setup do Projeto
    Tutor Controller

--- QuemMeAjuda/src/tutor/TutorController.java
+++ QuemMeAjuda/src/tutor/TutorController.java
@@ -1,5 +1,12 @@
 package tutor;
 
+import java.util.Map;
+
+import aluno.Alunado;
+import aluno.Aluno;
+
+import java.util.HashMap;
+
 /**
  * 
  * [DESCRIÃ‡ÃƒO DO QUE A CLASSE Ã‰, REPRESENTA E OUTRAS INFORMAÃ‡Ã•ES IMPORTANTES]
@@ -15,4 +22,64 @@ package tutor;
  */
 public class TutorController {
 
+	/*
+	 * Representa uma lista de tutores.
+	 */
+	private Map<String, Aluno> tutores;
+	
+	/*
+	 * Representa o construtor do Controller de Tutor.
+	 */
+	public TutorController() {
+		this.tutores = new HashMap<String, Aluno>();
+	}
+	
+	/*
+	 * Metodo responsavel por assumir a função de Tutor para um aluno.
+	 * 
+	 * @param Aluno
+	 * 			Aluno que assumirá a função de tutor.
+	 * 
+	 * @param disciplina
+	 * 			A disciplina que o aluno virará um tutor.
+	 * 
+	 * @param proficiencia
+	 * 			A proficiencia do aluno na disciplina.
+	 */
+	public void tornaTutor(Aluno aluno, String disciplina, int proficiencia) {
+		tutores.put(aluno.getMatricula(), aluno);
+		aluno.tornarTutor(disciplina, proficiencia);
+	}
+	
+	/*
+	 * Metodo responsavel por retornar a descricao textual de um Tutor.
+	 * 
+	 * A descrição segue o modelo:
+	 * 
+	 * @param matricula
+	 * 			Matricula do tutor em questão.
+	 * 
+	 * @return String A descrição textual do tutor.
+	 */
+	public String recuperaTutor(String matricula) {
+		return this.tutores.get(matricula).gerarDetalhes();
+	}
+	
+	/*
+	 * Método responsável por retornar uma lista de todos os tutores.
+	 * 
+	 * @return tutores
+	 * 		Uma lista com todos os Tutores registrados.
+	 */
+	public String listarTutores() {
+		String lista = "";
+		
+		for(Aluno aluno : tutores.values()) {
+			lista += aluno.toString() + "\n";
+		}
+		
+		return lista;
+	}
+	
+	 
 }
